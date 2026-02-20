127e82d785994f0caae51c9e0e94a8934606be91
Kiran R <kiran.r1497@gmail.com>
2026-02-20 20:28:11 +0530

    third commit - login page frontend

---
+++ Public/login.js
@@ -0,0 +1,34 @@
+async function handleLogin(event){
+    event.preventDefault(); 
+
+    const loginDetails = {
+        email: event.target.email.value, 
+        password: event.target.password.value
+    }
+
+    try {
+        const response = await axios.post("http://localhost:3000/user/login",
+            loginDetails);
+
+            if(response.status == 200){
+                alert("login successful!"); 
+            }
+    } catch (error) {
+        if(error.response){
+             if(error.response.status === 404){
+                alert("User not found (Email is wrong)");
+            }
+
+            else if(error.response.status === 400){
+                alert("Incorrect password");
+            }
+
+            else{
+                alert("Something went wrong");
+            }
+        }
+        else {
+            alert("Server not reachable");
+        }
+    }
+}
